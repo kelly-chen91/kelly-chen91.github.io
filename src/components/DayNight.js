6d6d32b86c5dfb047bba36607a1c4820df3ce614
Kelly Chen <kellychen966@gmail.com>
2024-01-14 22:05:58 -0500

    day night mode in progress

--- src/components/DayNight.js
+++ src/components/DayNight.js
@@ -1,10 +1,26 @@
+import * as React from 'react'
 import FormGroup from '@mui/material/FormGroup'
 import { FormControlLabel } from '@mui/material'
 import Switch from '@mui/material/Switch'
+import useMediaQuery from 'react-responsive'
+// import { useState } from 'react';
 export default function BackgroundButton() {
+    const [isDarkMode, setDarkMode] = React.useState(false);
+    const systemPrefersDark = useMediaQuery(
+        {
+            query: "(prefers-color-scheme: dark)",
+        },
+        undefined,
+        (isSystemDark) => setDarkMode(isSystemDark)
+    );
     return (
         <FormGroup className='nightModeContainer'>
-            <FormControlLabel className='nightMode' control={<Switch />} label="Night Mode" />
+            <FormControlLabel className='nightMode' control={
+                <Switch
+                    checked={isDarkMode}
+                    onChange={({ target }) => setDarkMode(target.checked)}
+                    icons={{ checked: "🌙", unchecked: "🔆" }} />
+            } label="Night Mode" />
         </FormGroup>
     );
 }
